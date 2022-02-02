4e048830c9c14891893ebd2654af3c874cf66b6f
Lúcio Benelli Pereira <luciobenelli@gmail.com>
2022-02-01 23:06:35 -0300

    adding survey controller.

---
+++ src/main/java/survey/api/SurveyDTO.java
@@ -0,0 +1,20 @@
+package survey.api;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SurveyDTO {
+
+    private Long id;
+
+    public static SurveyDTO build(Long id) {
+        return SurveyDTO.builder()
+                .id(id)
+                .build();
+    }
+
+}
